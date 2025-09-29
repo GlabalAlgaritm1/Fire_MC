f7c72a8553153b9d90dc052f4977c20c15dd8f25
GlabalAlgoritm1 <ahmadaliyevzafarbek02@gmail.com>
2025-09-29 22:26:27 +0500

    hayda

--- src/pages/Film.jsx
+++ src/pages/Film.jsx
@@ -16,7 +16,9 @@ const Film = () => {
         <img
           src={item.card1}
           alt="Film Card"
-          className="w-full max-w-[300px] h-[275px] border-b object-cover rounded-lg mb-3 max-md:h-[250px] max-sm:w-full  max-sm:max-w-[140px] max-sm:h-[200px] max-sm:object-cover "
+          className="w-full max-w-[300px] h-[275px] border-b object-cover rounded-lg mb-3
+                     max-md:h-[250px] 
+                     max-sm:w-full max-sm:h-[220px] max-sm:object-cover"
         />
         <span className="absolute bottom-[72px] right-2 font-semibold bg-white/10 text-white text-base p-1 rounded-md border border-white/20 opacity-100 group-hover:opacity-0 transition-opacity duration-300">
           {item.year}
@@ -37,7 +39,7 @@ const Film = () => {
   const renderSeeAll = (id) => (
     <li
       key={id}
-      className="flex flex-col items-center justify-center border border-white/20 rounded-lg hover:bg-white/20 transition-all duration-300 cursor-pointer h-[275px] max-sm:w-full max-sm:max-w-[200px] max-sm:h-screen max-sm:max-h-[200px]"
+      className="flex flex-col items-center justify-center border border-white/20 rounded-lg hover:bg-white/20 transition-all duration-300 cursor-pointer h-[275px] max-sm:w-full max-sm:max-w-[200px] max-sm:max-h-[220px]"
     >
       <Link to="/film" className="flex flex-col items-center justify-center gap-y-3">
         <span className="text-white text-xl font-semibold max-sm:text-base">Barchasini ko‘rish</span>
@@ -57,7 +59,7 @@ const Film = () => {
         <Swiper
           grabCursor={true}
           breakpoints={{
-            0: { slidesPerView: 2, slidesPerGroup: 2, spaceBetween: 4 },   // 📱 Telefon
+            0: { slidesPerView: 2, slidesPerGroup: 2, spaceBetween: 8 },   // 📱 Telefon
             640: { slidesPerView: 4, slidesPerGroup: 4, spaceBetween: 16 }, // 📲 Planshet
             1024: { slidesPerView: 6, slidesPerGroup: 6, spaceBetween: 20 } // 💻 Katta ekran
           }}
@@ -72,7 +74,7 @@ const Film = () => {
         <Swiper
           grabCursor={true}
           breakpoints={{
-            0: { slidesPerView: 2, slidesPerGroup: 2, spaceBetween: 4 },
+            0: { slidesPerView: 2, slidesPerGroup: 2, spaceBetween: 8 },
             640: { slidesPerView: 4, slidesPerGroup: 4, spaceBetween: 16 },
             1024: { slidesPerView: 6, slidesPerGroup: 6, spaceBetween: 20 }
           }}
